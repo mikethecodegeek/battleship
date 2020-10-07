2f2adb0a165e4d58fe427d899626901cabce6398
MeeraMeera1 <gatesameera98@gmail.com>
2020-10-07 13:40:20 -0400

    Created board

--- board.js
+++ board.js
@@ -1,7 +1,7 @@
-// 10 x 10 board 
+// 10 x 10 board
 // represent in a two dimensional array
 // ex [ [1,2,3,4,5,6,7,8,9,10]
-//      [1,2,3,4,5,6,7,8,9,10]    
+//      [1,2,3,4,5,6,7,8,9,10]
 
 //    ]
 // totaly do that in a for loop
@@ -12,5 +12,17 @@
 // hold ships in the bounderies
 // display it's state
 // undamaged ship with S and damaged with X or "" if nothing there
-// 
-
+//
+let board = [];
+const createEmptyBoard = function (size) {
+  let boardSize = 10;
+  for (let i = 0; i < boardSize; i++) {
+    let inner = [];
+    for (let z = 0; z < boardSize; z++) {
+      inner.push("");
+    }
+    board.push(inner);
+  }
+};
+createEmptyBoard();
+console.table(board);
