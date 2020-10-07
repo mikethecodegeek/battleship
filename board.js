73a70ec61646682f67b09d3e30740507221b392a
mike <mikethecodegeek@gmail.com>
2020-10-07 10:28:22 -0700

    maybe a plan

---
+++ board.js
@@ -0,0 +1,16 @@
+// 10 x 10 board 
+// represent in a two dimensional array
+// ex [ [1,2,3,4,5,6,7,8,9,10]
+//      [1,2,3,4,5,6,7,8,9,10]    
+
+//    ]
+// totaly do that in a for loop
+
+// functions of the board --
+// hold ship pieces
+// change when ship is hit
+// hold ships in the bounderies
+// display it's state
+// undamaged ship with S and damaged with X or "" if nothing there
+// 
+
