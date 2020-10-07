57c46d4b8e78054f5b32d2159c1b632a4cea9273
MeeraMeera1 <gatesameera98@gmail.com>
2020-10-07 16:32:00 -0400

    Collison Managed

--- board.js
+++ board.js
@@ -14,59 +14,83 @@
 // undamaged ship with S and damaged with X or "" if nothing there
 //
 
-
 // attempt to place ship in coordinates
 // if other ships or sides are in way we cannot place the ship
 // else make the array filled with S's where the ship is
 
 class Board {
-    constructor(){
-        this.board = [];
+  constructor() {
+    this.board = [];
+  }
+
+  createEmptyBoard(size) {
+    let boardSize = 10;
+    for (let i = 0; i < boardSize; i++) {
+      let inner = [];
+      for (let z = 0; z < boardSize; z++) {
+        inner.push("");
+      }
+      this.board.push(inner);
     }
+  }
 
-    createEmptyBoard (size) {
-        let boardSize = 10;
-        for (let i = 0; i < boardSize; i++) {
-          let inner = [];
-          for (let z = 0; z < boardSize; z++) {
-            inner.push("");
+  showBoard() {
+    console.table(this.board);
+  }
+
+  boundaries = (x, y, shiplength, orientation) => {
+      let myLength = shiplength;
+    if (x + shiplength > this.board.length && orientation !== "horiz") {
+      return false;
+    } else if (y + shiplength > this.board.length && orientation == "horiz") {
+      return false;
+    } else {
+      if (orientation == "horiz") {
+        for (let a = 0; a < myLength; a++) {
+          if (this.board[x][y] !== "") {
+            return false;
           }
-          this.board.push(inner);
+          y++;
         }
-      };
-
-      showBoard() {
-          console.table(this.board)
+      } else {
+        for (let a = 0; a < myLength; a++) {
+          if (this.board[x][y] !== "") {
+            return false;
+          }
+          x++;
+        }
       }
 
-      placeShip = (x,y,shipLength, orientation) => {
-          if (orientation == 'horiz') {
-              for (let a=0; a< shipLength; a++) {
-                  this.board[x][y] = 'S';
-                  y++;
-              }
-          } else {
-            for (let a=0; a< shipLength; a++) {
-                this.board[x][y] = 'S';
-                x++;
-            }
-          }
+      return true;
     }
-        boundaries() {
-            // return true if we can place a ship else return false
-            // loop through the possible coordinates and see if there is a ship present
-            // if there is a ship immediately return false
-            // if we find no ship return true
-            // also if place horiz - check and see if x+ shiplength > board length
-            // if place vert - check and see if y+shiplength > board length
+  };
 
-            
+  placeShip = (x, y, shipLength, orientation) => {
+    if (this.boundaries(x, y, shipLength, orientation) == true) {
+      if (orientation == "horiz") {
+        for (let a = 0; a < shipLength; a++) {
+          this.board[x][y] = "S";
+          y++;
         }
+      } else {
+        for (let a = 0; a < shipLength; a++) {
+          this.board[x][y] = "S";
+          x++;
+        }
+      }
+    }
+  };
+  // return true if we can place a ship else return false
+  // loop through the possible coordinates and see if there is a ship present
+  // if there is a ship immediately return false
+  // if we find no ship return true
+  // also if place horiz - check and see if x+ shiplength > board length
+  // if place vert - check and see if y+shiplength > board length
 }
 
-let game = new Board;
+let game = new Board();
 game.createEmptyBoard();
-game.placeShip(5,5,3,'horiz');
+game.placeShip(5, 5, 3, "horiz");
 
-game.placeShip(2,7,3,'vert')
-game.showBoard()
+game.placeShip(5,5,3, "vert");
+game.showBoard();
